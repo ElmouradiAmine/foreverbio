efbe8fc6a043f7815de6efc32ffc7fa4b2cbd319
ElmouradiAmine <elmouradi.amine98@gmail.com>
2020-03-01 16:54:57 +0100

    Added the method that returns a user, using the email and the password.
    springboot automatically handles the generation of this method. Lucky
    us.

--- src/main/java/com/itwins/foreverbio/repository/UserRepository.java
+++ src/main/java/com/itwins/foreverbio/repository/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.itwins.foreverbio.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
+	
+	public User findByEmailAndPassword(String email, String password);
 
 }
  
