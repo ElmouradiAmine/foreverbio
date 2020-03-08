0f5c5f8267a26c297b6cc4e58b844f8c4bcc39e0
Imane <imaneelmouradi98@gmail.com>
2020-03-08 12:44:28 +0100

    add: comments in ProductController.java

--- src/main/java/com/itwins/foreverbio/controllers/ProductController.java
+++ src/main/java/com/itwins/foreverbio/controllers/ProductController.java
@@ -19,66 +19,97 @@ import com.itwins.foreverbio.models.Product;
 @org.springframework.web.bind.annotation.RestController
 public class ProductController {
 
-	@Autowired 
-	private ProductService productService;
-	
-	@GetMapping("/product")
-    public List<Product> index(){
+    @Autowired
+    private ProductService productService;
+
+    // ! @route GET /product
+    // ! @desc shows all products in mysql database
+    // ! @access public
+    @GetMapping("/product")
+    public List<Product> index() {
         return productService.findAll();
     }
 
+    // ! @route GET /product/id
+    // ! @desc finds and shows product with parameter id
+    // ! @access public
     @GetMapping("/product/{id}")
-    public Optional<Product> show(@PathVariable String id){
+    public Optional<Product> show(@PathVariable String id) {
         int productId = Integer.parseInt(id);
         return productService.findById(productId);
     }
 
+    // ! @route POST /product/search
+    // ! @desc uses body parameter : "text" and finds a product with
+    // name or description that contains the text input
+    // ! @access public
     @PostMapping("/product/search")
-    public List<Product> search(@RequestBody Map<String, String> body){
+    public List<Product> search(@RequestBody Map<String, String> body) {
         String searchTerm = body.get("text");
         return productService.findByNomContainingOrDescriptionContaining(searchTerm, searchTerm);
     }
 
+    // ! @route POST /product
+    // ! @desc adds product in database. Body parameters needed :
+    // id, nom, description, source, etat, prix, qte
+    // exemple :
+    // {
+    // "id":"5",
+    // "nom":"produit1",
+    // "description":"description produit1",
+    // "source":"source1",
+    // "etat":"vente",
+    // "prix":"100.00",
+    // "qte":"34"
+    // }
+    // ! @access public (for now- it should be accessible by admins only)
     @PostMapping("/product")
-    public Product create(@RequestBody Map<String, String> body){ 
-    	int id = Integer.parseInt(body.get("id"));
+    public Product create(@RequestBody Map<String, String> body) {
+        int id = Integer.parseInt(body.get("id"));
         String nom = body.get("nom");
         String description = body.get("description");
         String source = body.get("source");
         String etat = body.get("etat");
         double prix = Double.parseDouble(body.get("prix"));
         int qte = Integer.parseInt(body.get("qte"));
-        
+
         return productService.saveProduct(new Product(id, nom, description, source, etat, prix, qte));
     }
 
+    // ! @route PUT /product/id
+    // ! @desc modifies product in database. Body parameters needed :
+    // id, nom,description, source, etat, prix, qte
+    // ! @access public (for now- it should be accessible by admins only)
     @PutMapping("/product/{id}")
-    public Product update(@PathVariable String id, @RequestBody Map<String, String> body){
+    public Product update(@PathVariable String id, @RequestBody Map<String, String> body) {
         int productId = Integer.parseInt(id);
         Optional<Product> product = productService.findById(productId);
-        if(product.isPresent()) {
-        	Product p = product.get();
-        	p.setNom(body.get("nom"));
+        if (product.isPresent()) {
+            Product p = product.get();
+            p.setNom(body.get("nom"));
             p.setDescription(body.get("description"));
             p.setSource(body.get("source"));
             p.setEtat(body.get("etat"));
             p.setPrix(Double.parseDouble(body.get("prix")));
             p.setQte(Integer.parseInt(body.get("qte")));
-            
+
             return productService.saveProduct(p);
         }
-         return null;
+        return null;
     }
 
+    // ! @route DELETE /product/id
+    // ! @desc deletes product with param id
+    // ! @access public (for now- it should be private for admins)
     @DeleteMapping("product/{id}")
-    public boolean delete(@PathVariable String id){
+    public boolean delete(@PathVariable String id) {
         int productId = Integer.parseInt(id);
         try {
-        	productService.deleteById(productId);
+            productService.deleteById(productId);
             return true;
-        	
-        } catch(Exception e ) {
-        	return false;
+
+        } catch (Exception e) {
+            return false;
         }
     }
 }
