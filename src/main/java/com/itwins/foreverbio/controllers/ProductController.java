bbfa2b428ea9e06ef9ae1e283ce323c620dde6a4
El Mouradi Imane <imaneelmouradi98@gmail.com>
2020-03-08 13:14:59 +0100

    Crud products (#9)
    * add: crud product
    
    * add: comments in ProductController.java

---
+++ src/main/java/com/itwins/foreverbio/controllers/ProductController.java
@@ -0,0 +1,115 @@
+package com.itwins.foreverbio.controllers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.itwins.foreverbio.services.ProductService;
+
+import com.itwins.foreverbio.models.Product;
+
+@org.springframework.web.bind.annotation.RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    // ! @route GET /product
+    // ! @desc shows all products in mysql database
+    // ! @access public
+    @GetMapping("/product")
+    public List<Product> index() {
+        return productService.findAll();
+    }
+
+    // ! @route GET /product/id
+    // ! @desc finds and shows product with parameter id
+    // ! @access public
+    @GetMapping("/product/{id}")
+    public Optional<Product> show(@PathVariable String id) {
+        int productId = Integer.parseInt(id);
+        return productService.findById(productId);
+    }
+
+    // ! @route POST /product/search
+    // ! @desc uses body parameter : "text" and finds a product with
+    // name or description that contains the text input
+    // ! @access public
+    @PostMapping("/product/search")
+    public List<Product> search(@RequestBody Map<String, String> body) {
+        String searchTerm = body.get("text");
+        return productService.findByNomContainingOrDescriptionContaining(searchTerm, searchTerm);
+    }
+
+    // ! @route POST /product
+    // ! @desc adds product in database. Body parameters needed :
+    // id, nom, description, source, etat, prix, qte
+    // exemple :
+    // {
+    // "id":"5",
+    // "nom":"produit1",
+    // "description":"description produit1",
+    // "source":"source1",
+    // "etat":"vente",
+    // "prix":"100.00",
+    // "qte":"34"
+    // }
+    // ! @access public (for now- it should be accessible by admins only)
+    @PostMapping("/product")
+    public Product create(@RequestBody Map<String, String> body) {
+        int id = Integer.parseInt(body.get("id"));
+        String nom = body.get("nom");
+        String description = body.get("description");
+        String source = body.get("source");
+        String etat = body.get("etat");
+        double prix = Double.parseDouble(body.get("prix"));
+        int qte = Integer.parseInt(body.get("qte"));
+
+        return productService.saveProduct(new Product(id, nom, description, source, etat, prix, qte));
+    }
+
+    // ! @route PUT /product/id
+    // ! @desc modifies product in database. Body parameters needed :
+    // id, nom,description, source, etat, prix, qte
+    // ! @access public (for now- it should be accessible by admins only)
+    @PutMapping("/product/{id}")
+    public Product update(@PathVariable String id, @RequestBody Map<String, String> body) {
+        int productId = Integer.parseInt(id);
+        Optional<Product> product = productService.findById(productId);
+        if (product.isPresent()) {
+            Product p = product.get();
+            p.setNom(body.get("nom"));
+            p.setDescription(body.get("description"));
+            p.setSource(body.get("source"));
+            p.setEtat(body.get("etat"));
+            p.setPrix(Double.parseDouble(body.get("prix")));
+            p.setQte(Integer.parseInt(body.get("qte")));
+
+            return productService.saveProduct(p);
+        }
+        return null;
+    }
+
+    // ! @route DELETE /product/id
+    // ! @desc deletes product with param id
+    // ! @access public (for now- it should be private for admins)
+    @DeleteMapping("product/{id}")
+    public boolean delete(@PathVariable String id) {
+        int productId = Integer.parseInt(id);
+        try {
+            productService.deleteById(productId);
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/itwins/foreverbio/models/Product.java
@@ -0,0 +1,101 @@
+package com.itwins.foreverbio.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class Product {
+	@Id
+	int id;
+	String nom;
+	String description;
+	String source;
+	String etat;
+	double prix;
+	int qte;
+	
+	public Product() {
+		
+	}
+
+	public Product(int id, String nom, String description, String source, String etat, double prix, int qte) {
+		super();
+		this.setId(id);
+		this.setNom(nom);
+		this.setDescription(description);
+		this.setSource(source);
+		this.setEtat(etat);
+		this.setPrix(prix);
+		this.setQte(qte);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getEtat() {
+		return etat;
+	}
+
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getQte() {
+		return qte;
+	}
+
+	public void setQte(int qte) {
+		this.qte = qte;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", nom=" + nom + ", description=" + description + ", source=" + source + ", etat="
+				+ etat + ", prix=" + prix + ", qte=" + qte + "]";
+	}
+	
+	
+	
+	
+	
+
+}
